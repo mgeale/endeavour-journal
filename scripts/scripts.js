871babb8d80daf634843b9fb035da35fdef39300
Matt Geale <matthewgeale@me.com>
2016-08-09 21:09:10 +1000

    Re-order code positions

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,8 @@
 Polymer({
-    is: "test-elem",
+    is: "index-elem",
     _on_response: function(r) {
         response = r.detail.response;
-        // list = response.querySelectorAll('day');
+        // list = response.querySelectorAll('day'); <!-- for xml version -->
         list = response;
         console.log(list);
 
