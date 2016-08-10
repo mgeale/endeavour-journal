0be6473c8b62ef35bf3a21b767c4831ac4c3b599
Matt Geale <matthewgeale@me.com>
2016-08-10 18:52:31 +1000

    Added marker array loop

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,1 @@
-Polymer({
-    is: "index-elem",
-    _on_response: function(r) {
-        response = r.detail.response;
-//         list = response.querySelectorAll('day'); // for xml version
-        list = response;
-        console.log(list);
 
-    }
-});
