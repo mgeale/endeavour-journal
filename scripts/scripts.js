95a332659ebba618f089d498e36210031deca790
mgeale <matthewgeale@me.com>
2016-08-10 15:56:07 +1000

    Began adding Google Map markers

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,7 +2,7 @@ Polymer({
     is: "index-elem",
     _on_response: function(r) {
         response = r.detail.response;
-        // list = response.querySelectorAll('day'); <!-- for xml version -->
+//         list = response.querySelectorAll('day'); // for xml version
         list = response;
         console.log(list);
 
